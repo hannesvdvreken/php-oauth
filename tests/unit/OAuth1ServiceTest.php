b3a0760c09651a241a85892e72b78c5f227b46e5
Hannes Van De Vreken <vandevreken.hannes@gmail.com>
2014-08-13 23:30:33 +0200

    First tests for oauth service base class

---
+++ tests/unit/OAuth1ServiceTest.php
@@ -0,0 +1,18 @@
+<?php
+
+use OAuth\OAuth1Service;
+
+class OAuth1ServiceTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function stub()
+    {
+        // Arrange
+        $service = new OAuth1Service;
+
+        // Act
+        // Assert
+    }
+}
---
+++ tests/unit/OAuth2ServiceTest.php
@@ -0,0 +1,18 @@
+<?php
+
+use OAuth\OAuth2Service;
+
+class OAuth2ServiceTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function stub()
+    {
+        // Arrange
+        $service = new OAuth2Service;
+
+        // Act
+        // Assert
+    }
+}
