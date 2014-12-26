a81aec6fb9e89f4b06b277d2a67489f22dfa1762
Hannes Van De Vreken <vandevreken.hannes@gmail.com>
2014-12-26 21:44:12 +0100

    Added VKontakte thanks to @ftugit

---
+++ src/Services/Vkontakte.php
@@ -0,0 +1,27 @@
+<?php
+namespace OAuth\Services;
+
+use OAuth\OAuth2Service;
+
+class Vkontakte extends OAuth2Service
+{
+    /**
+     * @var string
+     */
+    protected $endpointAuthorization = 'https://oauth.vk.com/authorize';
+
+    /**
+     * @var string
+     */
+    protected $endpointAccessToken = 'https://oauth.vk.com/access_token';
+
+    /**
+     * @var string
+     */
+    protected $base = 'https://api.vk.com/method/';
+
+    /**
+     * @var string
+     */
+    protected $scopeDelimiter = ',';
+}
